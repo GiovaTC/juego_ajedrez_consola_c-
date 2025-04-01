e61b70ede60d5f9b4773829a1fd740682b5d70fc
gtapiero <giovanny.tapiero@pi.edu.co>
2025-04-01 17:16:08 -0500

    commit 2

--- Juego_ajedres_consola_c#/Program.cs
+++ Juego_ajedres_consola_c#/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http.Headers;
 
 class Program
 {
@@ -13,5 +14,115 @@ class Program
         { 'p','p', 'p', 'p', 'p', 'p', 'p', 'p' },
         { 'r','n', 'b', 'q', 'k', 'b', 'n', 'r' }
     };
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            PrintBoard();
+            Console.Write("Ingrese la posicion de origen (ej. e2): ");
+            string from = Console.ReadLine();
+            Console.Write("Ingrese la posicion de origen (ej. e4): ");
+            string to = Console.ReadLine();
+
+            if (MovePiece(from, to))
+            {
+                Console.WriteLine("Movimiento realizado.");
+            }
+            else
+            {
+                Console.WriteLine("Movimiento invalido");
+            }
+            Console.ReadLine();
+        }
+    }
+    
+    static void PrintBoard()
+    {
+        Console.WriteLine(" a b c d e f g h");
+        for(int i = 0; i < 8; i++)
+        {
+            Console.Write(8 - i + " ");
+            for(int j = 0; j < 8; j++)
+            {
+                Console.Write(board[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static bool MovePiece(string from, string to)
+    {
+        int fromRow = 8 - (from[1] - '0');
+        int fromCol = from[0] - 'a';
+        int toRow = 8 - (to[1] - '0');
+        int toCol = to[0] - 'a';
+
+        if (IsValidMove(fromRow, fromCol, toRow, toCol))
+        {
+            board[toRow, toCol] = board[fromRow, fromCol];
+            board[fromRow, fromCol] = '.';
+            return true;
+        }
+        return false;
+    }
+
+    static bool IsValidMove(int fromRow, int fromCol, int toRow, int toCol)
+    {
+        if(fromRow < 0 || fromRow >= 8 || fromCol < 0 || fromCol >= 8 || 
+           toRow < 0 || toRow >= 8 || toCol < 0 || toCol >= 8 )
+        {
+            return false;
+        }
+
+        char piece = board[fromRow, fromCol];
+        char target = board[toRow, toCol];
+
+        if (piece == '.') return false;
+        if (char.IsUpper(piece) == char.IsUpper(target)) return false;
+
+        switch (char.ToLower(piece))
+        {
+            case 'p':
+                return ValidatePawnMove(fromRow, fromCol, toRow, toCol, char.IsUpper(piece));
+            case 'r':
+                return ValidateRookMove(fromRow, fromCol, toRow, toCol);
+            case 'n':
+                return ValidateKnightMove(fromRow, fromCol, toRow, toCol);
+            case 'b':
+                return ValidateBishopMove(fromRow, fromCol, toRow, toCol);
+            case 'q':
+                return ValidateQueenMove(fromRow, fromCol, toRow, toCol);
+            case 'k':
+                return ValidateKingMove(fromRow, fromCol, toRow, toCol);
+            default:
+                return false;
+        }
+    }
+
+    static bool ValidatePawnMove(int fromRow, int fromCol, int toRow, int toCol, bool isWhite)
+    {
+        int direction = isWhite ? -1 : 1;
+        if (fromCol == toCol && board[toRow, toCol] == '.')
+        {
+            if (toRow == fromRow + direction) return true;
+            if ((isWhite && fromRow == 6 || !isWhite && fromRow == 1) && toRow == fromRow + 2 * direction && board[fromRow + direction, fromCol] == '.') return true;
+        }
+        else if (Math.Abs(fromCol - toCol) == 1 && toRow == fromRow + direction && board[toRow, toCol] != '.')
+        {
+            return true;
+        }
+        return false;
+    }
+
+    static bool ValidateRookMove(int fromRow, int fromCol, int toRow, int toCol)
+    {
+        int rowDiff = Math.Abs(fromRow - toRow);
+        int colDiff = Math.Abs(fromCol - toCol);
+        return (rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2);
+
+    }
+
 
 }
